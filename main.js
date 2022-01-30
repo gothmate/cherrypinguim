d930af3745036eb619794b3082f277986bf2ed45
Gothmate <gothmate@gmail.com>
2022-01-30 18:45:23 -0300

    correção de link do modal

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 const hamburguer = document.querySelector('.hamburguer')
-const hamburguer_container = document.querySelector('.hamburguer_container')
 const modal = document.querySelector('#modal')
+const links = document.querySelectorAll('.links')
+const container = document.querySelector('.hamburguer_container')
 
 hamburguer.addEventListener('click', e => {
   if (e.target.classList.value == 'hamburguer') {
     hamburguer.classList.add('animate')
     hamburguer.classList.remove('hamburguer')
+    links.forEach(link => {
+      console.log(link)
+      link.classList.remove('disable')
+    })
+
     modal.style.opacity = 1
   } else {
     hamburguer.classList.add('hamburguer')
     hamburguer.classList.remove('animate')
     modal.style.opacity = 0
+    links.forEach(link => {
+      link.classList.add('disable')
+    })
   }
   modal.addEventListener('click', e => {
     hamburguer.classList.add('hamburguer')
     hamburguer.classList.remove('animate')
     modal.style.opacity = 0
+    links.forEach(link => {
+      link.classList.add('disable')
+    })
   })
 })
